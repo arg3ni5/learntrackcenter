a0b0abee0804c5306674d24e7d5d614eb9da90dc
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2024-12-30 15:10:12 -0600

    feat: add TypeScript interfaces for courses, assignments, periods, and students

---
+++ src/types/types.ts
@@ -0,0 +1,77 @@
+export interface Assignment {
+    id?: string; // Optional ID property, used when fetching from Firestore
+    title: string; // Title of the assignment
+    grade: number; // Grade obtained for the assignment
+    contributionPercentage: number; // Contribution percentage to final grade
+}
+export type CourseStatus = 
+    | 'Passed' 
+    | 'Failed' 
+    | 'In Progress' 
+    | 'Not Started' 
+    | 'Withdrawn';
+
+
+
+export interface AvailableCourse {
+    id?: string; // Optional ID for Firestore
+    name: string; // Course title
+    description: string; // Course description
+    duration?: number; // Course duration in weeks
+    hours?: number; // Hours per week
+}
+
+export interface Course extends AvailableCourse{
+    status?: CourseStatus; // Approved or Reprobated status
+    courseId: string; // Reference to the Course document
+    assignments: string[]; // List of assignments for the course
+}
+
+export interface StudentCourse extends Omit<AvailableCourse, 'duration' | 'hours'>{
+    courseId: string; // Reference to the Course document
+    periodId: string; // Reference to the Period document
+    finalGrade: number; // Final grade for the course
+    status: CourseStatus; // Approved or Reprobated status
+    assignments: string[]; // List of assignments for the course
+}
+
+
+
+export interface CourseWithDetails extends Course {
+
+}
+
+export type PeriodStatus = 
+    | 'Active' 
+    | 'Finished' 
+    | 'In Progress' 
+    | 'Upcoming';
+
+// Define the interface for Period
+export interface Period {
+    id?: string; // Optional ID for Firestore
+    name: string; // Descriptive name of the period
+    startDate: Date; // Start date of the academic period
+    endDate: Date; // End date of the academic period
+    status: PeriodStatus; // Current status of the period (e.g., "Active", "Finished", "Upcoming")
+    coursesIds: string[]; // List of courses IDs for the period
+}
+
+export interface PeriodWithDetails extends Period {
+    name: string; // Name of the course
+}
+
+export interface Student {
+    id?: string; // Optional ID property, used when fetching from Firestore
+    fullName: string; // Full name of the student
+    identificationNumber?: string; // Unique identification number for the student
+    email?: string; // Email address of the student
+    periods: string[]; // List of periods for the student (renamed from periodos to periods)
+}
+
+
+export interface AssignmentsManagerProps {
+    studentId: string;
+    periodId: string;
+    courseId: string;
+}
