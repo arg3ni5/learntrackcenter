3f8decc0c7967d5dd50406a889327e9d1ff8ff9e
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-01-04 19:28:11 -0600

    feat: add PeriodStudents page and integrate it into routing for period-specific student management

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -7,8 +7,6 @@ import PrivateRoute from "../modules/userAuth/components/PrivateRoute";
 import PeriodCourses from "../pages/PeriodCourses";
 import Loading from "../components/loading/Loading";
 import { useLoading } from "../components/loading/LoadingContext";
-import StudentsCourses from "../pages/StudentsCourses";
-import CourseStudents from "../pages/CourseStudents";
 
 const Login = lazy(() => import("../components/Login"));
 const Register = lazy(() => import("../components/Register"));
@@ -20,12 +18,15 @@ const Dashboard = lazy(() => import("../pages/Dashboard"));
 const Students = lazy(() => import("../pages/Students"));
 const Teachers = lazy(() => import("../pages/Teachers"));
 
+const StudentsCourses = lazy(() => import("../pages/StudentsCourses"));
+const CourseStudents = lazy(() => import("../pages/CourseStudents"));
+const PeriodStudents = lazy(() => import("../pages/PeriodStudents"));
+
 const AppRoutes: React.FC = () => {
   const { loading: authLoading } = useAuth();
   const { setIsLoading } = useLoading();
   const [initialLoadingComplete, setInitialLoadingComplete] = useState(false);
   const [showContent, setShowContent] = useState(false);
-
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
@@ -35,7 +36,7 @@ const AppRoutes: React.FC = () => {
       timer = setTimeout(() => {
         setIsLoading(false);
         setInitialLoadingComplete(true);
-        setShowContent(true)
+        setShowContent(true);
       }, 2000);
     }
 
@@ -50,30 +51,26 @@ const AppRoutes: React.FC = () => {
 
   const routes = (
     <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-        <Route path="/students" element={<PrivateRoute element={<Students />} />} />
-        <Route path="/students/:id/periods" element={<PrivateRoute element={<StudentsCourses />} />} />
-        <Route path="/students/:id/courses" element={<PrivateRoute element={<StudentsCourses />} />} />
-        <Route path="/teachers" element={<PrivateRoute element={<Teachers />} />} />
-        <Route path="/courses" element={<PrivateRoute element={<Courses />} />} />
-        <Route path="/grades" element={<PrivateRoute element={<Grades />} />} />
-        <Route path="/periods" element={<PrivateRoute element={<Periods />} />} />
-        <Route path="/periods/courses" element={<PrivateRoute element={<Periods />} />} />
-        <Route path="/period/:id/courses" element={<PrivateRoute element={<PeriodCourses />} />} />
-        <Route path="/period/:periodId/course/:courseId" element={<PrivateRoute element={<CourseStudents />} />} />
-        <Route path="/course/:id/students" element={<PrivateRoute element={<CourseStudents />} />} />
-        <Route path="*" element={<Navigate to="/" />} /> {/* Redirigir a la página de inicio */}
-      </Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+      <Route path="/students" element={<PrivateRoute element={<Students />} />} />
+      <Route path="/students/:id/periods" element={<PrivateRoute element={<StudentsCourses />} />} />
+      <Route path="/students/:id/courses" element={<PrivateRoute element={<StudentsCourses />} />} />
+      <Route path="/teachers" element={<PrivateRoute element={<Teachers />} />} />
+      <Route path="/courses" element={<PrivateRoute element={<Courses />} />} />
+      <Route path="/grades" element={<PrivateRoute element={<Grades />} />} />
+      <Route path="/periods" element={<PrivateRoute element={<Periods />} />} />
+      <Route path="/periods/courses" element={<PrivateRoute element={<Periods />} />} />
+      <Route path="/period/:id/courses" element={<PrivateRoute element={<PeriodCourses />} />} />
+      <Route path="/period/:id/students" element={<PrivateRoute element={<PeriodStudents />} />} />
+      <Route path="/period/:periodId/course/:courseId" element={<PrivateRoute element={<CourseStudents />} />} />
+      <Route path="/course/:id/students" element={<PrivateRoute element={<CourseStudents />} />} />
+      <Route path="*" element={<Navigate to="/" />} /> {/* Redirigir a la página de inicio */}
+    </Routes>
   );
 
-  return (
-    <Suspense fallback={<Loading text="Cargando componente..."></Loading>}>
-      {showContent ? routes : null}
-    </Suspense>
-  );
+  return <Suspense fallback={<Loading text="Cargando componente..."></Loading>}>{showContent ? routes : null}</Suspense>;
 };
 
 export default AppRoutes;
-
