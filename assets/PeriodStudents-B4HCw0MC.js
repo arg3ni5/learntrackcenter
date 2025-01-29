04b5db6c1fcc379cc053038ff94193f158fb2533
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-01-29 08:09:21 -0600

    refactor: update Course interface to require enrolledStudents and enhance CourseCard logic for deletion

---
+++ assets/PeriodStudents-B4HCw0MC.js
@@ -0,0 +1,1 @@
+import{n as e,j as r,P as t}from"./index-BaZKZUog.js";const d=()=>{const{id:s}=e();return r.jsx("div",{children:s&&r.jsx(t,{periodId:s})})};export{d as default};
--- assets/PeriodStudents-BmdHmFzK.js
+++
@@ -1,1 +0,0 @@
-import{q as e,j as r,P as t}from"./index-BozJdoqh.js";const d=()=>{const{id:s}=e();return r.jsx("div",{children:s&&r.jsx(t,{periodId:s})})};export{d as default};
