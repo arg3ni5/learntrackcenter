2cc9ebf242db0189c7e18cc188f0360938bb457c
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-02-02 13:53:54 -0600

    refactor: reorganize CSS files into a dedicated directory for better structure and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/variables.css'
-import './styles/animates.css'
-import './styles/buttons.css'
+import './styles/css/variables.css'
+import './styles/css/animates.css'
+import './styles/css/buttons.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
