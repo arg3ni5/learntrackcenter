15d424537dd4a0edc7af118d45d6680e6fed3706
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-02-06 15:10:19 -0600

    feat: enhance periods management with notification support and improved error handling

--- src/modules/periodsManagement/hooks/usePeriods.ts
+++ src/modules/periodsManagement/hooks/usePeriods.ts
@@ -1,61 +1,83 @@
 // src/modules/periodsManagement/hooks/usePeriods.ts
 
-import { useState, useEffect } from 'react';
-import { fetchPeriods, addPeriod, deletePeriod, updatePeriod } from '../services/periodService';
-import { Period } from '../../../types/types';
-
+import { useState, useEffect } from "react";
+import { fetchPeriods, addPeriod, deletePeriod, updatePeriod } from "../services/periodService";
+import { Period } from "../../../types/types";
+import { useNotification } from "../../../components/notification/NotificationContext";
 
 const usePeriods = () => {
-    const [periods, setPeriods] = useState<Period[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+  const [periods, setPeriods] = useState<Period[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const { showNotification } = useNotification();
 
-    const loadPeriods = async () : Promise<Period[]> => {
-        try {
-            setLoading(true);
-            const fetchedPeriods = await fetchPeriods();
-            setPeriods(fetchedPeriods);
-            return fetchedPeriods;
-        } catch (err) {
-            setError('Error fetching periods');
-            return [];
-        } finally {
-            setLoading(false);
-        }
-    };
+  const loadPeriods = async (): Promise<Period[]> => {
+    try {
+      setLoading(true);
+      const fetchedPeriods = await fetchPeriods();
+      setPeriods(fetchedPeriods);
+      return fetchedPeriods;
+    } catch (err) {
+      setError("Error fetching periods");
+      return [];
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        loadPeriods();
-    }, []);
+  useEffect(() => {
+    loadPeriods();
+  }, []);
 
-    const handleAddPeriod = async (newPeriod: Period) => {
-        try {
-            await addPeriod(newPeriod);
-            loadPeriods();
-        } catch (err) {
-            setError('Error adding period');
-        }
-    };
+  useEffect(() => {
+    error && showNotification(error, "error");
+  }, [error]);
 
-    const handleDeletePeriod = async (id: string) => {
-        try {
-            await deletePeriod(id);
-            loadPeriods();
-        } catch (err) {
-            setError('Error deleting period');
-        }
-    };
+  const handleAddPeriod = async (newPeriod: Period) => {
+    try {
+      const addedPeriod = await addPeriod(newPeriod);
+      if (addedPeriod.id) {
+        setPeriods((prevPeriods) => [...prevPeriods, addedPeriod]);
+      } else {
+        setError("Error adding period, no ID returned");
+      }
+    } catch (err) {
+      setError("Error adding period");
+    }
+  };
 
-    const handleUpdatePeriod = async (id: string, period: Period) => {
-        try {
-            await updatePeriod(id, period); // Eliminar curso por ID
-            loadPeriods(); // Recargar la lista de cursos después de eliminar
-        } catch (err) {
-            setError('Error updating period'); // Manejar errores
-        }
-    };
+  const handleDeletePeriod = async (id: string) => {
+    const periodToDelete = periods.find((period) => period.id === id);
 
-    return { periods, loadPeriods, loading, error, handleAddPeriod, handleDeletePeriod, handleUpdatePeriod};
+    if (!periodToDelete) {
+      setError("Period does not exist in the local state");
+      return;
+    }
+    if (periodToDelete && periodToDelete.coursesIds && periodToDelete.coursesIds.length > 0) {
+      setError("Cannot delete period: it has associated courses.");
+      return;
+    }
+    try {
+      const success = await deletePeriod(id); // Llamar al servicio para eliminar el periodo
+      if (success) {
+        showNotification("Item deleted", "success");
+        setPeriods((prevPeriods) => prevPeriods.filter((period) => period.id !== id)); // Eliminar del estado
+      }
+    } catch (err) {
+      setError("Error deleting period");
+    }
+  };
+
+  const handleUpdatePeriod = async (id: string, updatedPeriod: Period) => {
+    try {
+      await updatePeriod(id, updatedPeriod);
+      setPeriods((prevPeriods) => prevPeriods.map((period) => (period.id === id ? { ...period, ...updatedPeriod } : period)));
+    } catch (err) {
+      setError("Error updating period");
+    }
+  };
+
+  return { periods, loadPeriods, loading, error, handleAddPeriod, handleDeletePeriod, handleUpdatePeriod };
 };
 
 export default usePeriods;
--- src/modules/periodsManagement/services/periodService.ts
+++ src/modules/periodsManagement/services/periodService.ts
@@ -3,9 +3,9 @@
  * @module periodService
  */
 
-import { db } from '../../../services/firebase';
-import { collection, addDoc, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
-import { Period } from '../../../types/types';
+import { db } from "../../../services/firebase";
+import { collection, addDoc, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
+import { Period } from "../../../types/types";
 
 /**
  * Fetches all periods from Firestore.
@@ -13,13 +13,28 @@ import { Period } from '../../../types/types';
  * @returns {Promise<Period[]>} A promise that resolves to an array of Period objects.
  */
 export const fetchPeriods = async (): Promise<Period[]> => {
-    const periodsCollection = collection(db, 'periods');
-    const periodSnapshot = await getDocs(periodsCollection);
-    const periodList: Period[] = periodSnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data() as Omit<Period, 'id'>
-    }));
-    return periodList;
+  const periodsCollection = collection(db, "periods");
+  const periodSnapshot = await getDocs(periodsCollection);
+  const periodList: Period[] = periodSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as Omit<Period, "id">),
+  }));
+  return periodList;
+};
+
+/**
+ * Fetches all periods from Firestore.
+ * @async
+ * @returns {Promise<Period[]>} A promise that resolves to an array of Period objects.
+ */
+export const fetchPeriodsActive = async (): Promise<Period[]> => {
+  const periodsCollection = collection(db, "periods");
+  const periodSnapshot = await getDocs(periodsCollection);
+  const periodList: Period[] = periodSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as Omit<Period, "id">),
+  }));
+  return periodList;
 };
 
 /**
@@ -28,9 +43,10 @@ export const fetchPeriods = async (): Promise<Period[]> => {
  * @param {Period} period - The period object to be added.
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
-export const addPeriod = async (period: Period): Promise<void> => {
-    const periodsCollection = collection(db, 'periods');
-    await addDoc(periodsCollection, period);
+export const addPeriod = async (period: Period): Promise<Period> => {
+  const periodsCollection = collection(db, "periods");
+  const docRef = await addDoc(periodsCollection, period);
+  return { ...period, id: docRef.id };
 };
 
 /**
@@ -39,9 +55,15 @@ export const addPeriod = async (period: Period): Promise<void> => {
  * @param {string} id - The ID of the period to be deleted.
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
-export const deletePeriod = async (id: string): Promise<void> => {
-    const periodDoc = doc(db, 'periods', id);
+export const deletePeriod = async (id: string): Promise<boolean> => {
+  try {
+    const periodDoc = doc(db, "periods", id);
     await deleteDoc(periodDoc);
+    return true;
+  } catch (error) {
+    console.error("Error deleting period:", error);
+    throw new Error("Error deleting period");
+  }
 };
 
 /**
@@ -52,20 +74,19 @@ export const deletePeriod = async (id: string): Promise<void> => {
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
 export const updatePeriod = async (id: string, updatedPeriod: Partial<Period>): Promise<void> => {
-    const { id: _, ...period } = updatedPeriod;
-    const periodDoc = doc(db, 'periods', id);
-    await updateDoc(periodDoc, period);
-    syncCourses(id);
+  const { id: _, ...period } = updatedPeriod;
+  const periodDoc = doc(db, "periods", id);
+  await updateDoc(periodDoc, period);
+  syncCourses(id);
 };
 
-
 export const syncCourses = async (periodId: string): Promise<void> => {
-    const coursesCollection = collection(db, `periods/${periodId}/courses`);
-    const coursesSnapshot = await getDocs(coursesCollection);
-    const ids = coursesSnapshot.docs.map(doc => doc.id);
+  const coursesCollection = collection(db, `periods/${periodId}/courses`);
+  const coursesSnapshot = await getDocs(coursesCollection);
+  const ids = coursesSnapshot.docs.map((doc) => doc.id);
 
-    const periodDocRef = doc(db, `periods/${periodId}`);
-    await updateDoc(periodDocRef, {
-        coursesIds: ids
-    });
+  const periodDocRef = doc(db, `periods/${periodId}`);
+  await updateDoc(periodDocRef, {
+    coursesIds: ids,
+  });
 };
