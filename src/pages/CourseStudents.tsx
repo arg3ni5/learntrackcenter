25ad00ea30c9adef4935ee7b3469273b6b87f81e
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-09-11 00:25:46 -0600

    fix: cast course to PeriodCourse type in CourseStudents component

--- src/pages/CourseStudents.tsx
+++ src/pages/CourseStudents.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import useCourse from '../modules/coursesManagement/hooks/useCourse';
 import CourseManager from '../modules/coursesManagement/components/CourseManager';
+import { PeriodCourse } from '../types/types';
 
 const CourseStudents: React.FC = () => {
     const { periodId, courseId } = useParams<{ periodId: string, courseId: string}>();
@@ -9,7 +10,7 @@ const CourseStudents: React.FC = () => {
 
     return (
         <>
-            {!loading && course && <CourseManager periodId={periodId} periodCourse={course!}/>}
+            {!loading && course && <CourseManager periodId={periodId} periodCourse={course! as PeriodCourse}/>}
         </>
     );
 };
